9dded57999a8021d0356b68455ac6cdcb7eea410
AmanPateI <96079253+AmanPateI@users.noreply.github.com>
2022-09-11 16:33:04 -0400

    Add files via upload

---
+++ build/launch-tutorial09_vbo_indexing-Release.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/amanp/Desktop/ogl-master/tutorial09_vbo_indexing/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\amanp\Desktop\ogl-master\build\Release\tutorial09_vbo_indexing.exe"  
+pause
